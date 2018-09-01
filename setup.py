0ddce97e88d270a799e2388aa2ef17e673d3c9a2
noamloya <noamloya@gmail.com>
2018-09-01 14:58:34 +0300

    Created a basic setup.py.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Localize',
+    version='0.1dev',
+    packages=['python'], # TODO(loya) see if this name needs to be changed
+    scripts=['python/src/main'], # TODO(loya) validate and rename.
+    long_description='TBD', # TODO(loya)
+)
