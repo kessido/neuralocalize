6ed4c74a90704a9a43028c5b5201b5aa503d9cf2
Ido Kessler <t-idkess@microsoft.com>
2018-09-03 01:51:34 +0300

    Finished the class stucture. Added documentation.

--- python/src/prediction.py
+++ python/src/prediction.py
@@ -1,6 +1,7 @@
 """This code simulates the prediction code of the of the connectivity model.
 """
 
+import gzip
 import pickle
 
 import numpy as np
@@ -12,22 +13,8 @@ import utils.cifti_utils
 import utils.utils
 
 
-def add_one_to_matrix(mat):
-    """ Add a column of 1's
-    Usually needed for linear algebra.
-
-    :param mat: The original matrix
-    :return: The matrix with another 1's column, as it's first column.
-    """
-    shape = list(mat.shape)
-    shape[1] += 1
-    res = np.ones(shape)
-    res[1:] = mat
-    return res
-
-
 class Localizer:
-    """A class containing the localizer data.
+    """A class containing the localizer model data.
     """
 
     class FeatureExtractor:
@@ -39,13 +26,11 @@ class Localizer:
             Create a scaling factor of the cortical and sub cortical parts.
 
             :param subjects: The subjects
-            :param pca:
+            :param pca: the PCA  to use.
             """
             self.ctx_indices, self.sub_ctx_indices = \
                 utils.cifti_utils.get_cortex_and_sub_cortex_indices()
-            features = map(
-                lambda subject: feature_extraction.extract_features(subject, pca),
-                subjects)
+            features = feature_extraction.extract_features(subjects, pca)
             features = np.asarray(features)
             self.pca = pca
             # TODO(kess) check if this doesn't just return the same result as scaling by the whole thing.
@@ -54,32 +39,49 @@ class Localizer:
             self.scaler_sub_ctx = sklearn.preprocessing. \
                 StandardScaler().fit(features[:, self.sub_ctx_indices])
 
-        @staticmethod
-        def load(file_path):
-            return pickle.load(open(file_path, 'rb'))
+        def _scale(self, subjects_features):
+            """Scale the subject features using constant scaling factor.
 
-        def _scale(self, subjects_image):
-            subjects_image[self.ctx_indices] = self.scaler_ctx.transform(subjects_image[self.ctx_indices])
-            subjects_image[self.sub_ctx_indices] = self.scaler_sub_ctx.transform(subjects_image[self.sub_ctx_indices])
-            return subjects_image
+            :param subjects_features: The subjects' features to scale [n_sample, n_features].
+            :return: The scaled subject's features.
+            """
+            res = np.zeros(subjects_features.shape)
+            res[:, self.ctx_indices] = self.scaler_ctx.transform(subjects_features[:, self.ctx_indices])
+            res[:, self.sub_ctx_indices] = self.scaler_sub_ctx.transform(subjects_features[:, self.sub_ctx_indices])
+            return res
 
-        def extract(self, subject):
-            subject_image = self._scale(subject)
-            return feature_extraction.extract_features(
-                subject_image, subject_data.brain_maps, self.pca)
+        def extract(self, subjects):
+            """Extract the subject features.
+
+            :param subjects: The subjects to extract their features [n_subjects, n_data].
+            :return: The subjects' features.
+            """
+            res = np.asarray(feature_extraction.extract_features(subjects, self.pca))
+            return self._scale(res)
 
     class Predictor:
-        def __init__(self, subject_feature, subjects_task, pca):
-            self.spatial_features = feature_extraction.get_spatial_filters(pca)
-            betas = np.asarray(map(
-                lambda feature, task: self.get_beta(
-                    feature,
-                    task),
-                zip(subject_feature, subjects_task)
-            ), dtype=np.float32)
-            self.beta = np.mean(betas, axis=0)
+        """A class containing all the localizer predictor model data.
 
-        def get_beta(self, subject_feature, subject_task):
+            This allow injecting another model instead, as it uses fit(x,y) and predict(x).
+        """
+
+        def __init__(self, pca):
+            """Init the predictor.
+
+            :param pca: The pca to extract the spatial filtering from.
+                            This is later user to group indexes by their connectivity ICA,
+                            and combine them as their group only predictors.
+            """
+            self.beta = None
+            self.spatial_features = feature_extraction.get_spatial_filters(pca)
+
+        def _get_beta(self, subject_feature, subject_task):
+            """Get the prediction betas from psudo-inverse of ((beta @ [1 subject_features] = subject_task)).
+
+            :param subject_feature: The subject features.
+            :param subject_task: The subject task results.
+            :return: The subject betas.
+            """
             task = subject_task
             subject_feature = sklearn.preprocessing.normalize(subject_feature)
             betas = np.zeros(
@@ -87,25 +89,106 @@ class Localizer:
             for j in range(self.spatial_features.shape[1]):
                 ind = self.spatial_features[:, j] > 0
                 y = task[ind]
-                x = add_one_to_matrix(subject_feature[ind])
+                x = utils.utils.add_ones_column_to_matrix(subject_feature[ind])
                 betas[:, j] = np.linalg.pinv(x) @ y
             return betas
 
-        def predict(self, x):
-            return add_one_to_matrix(x) @ self.beta
+        def fit(self, subjects_feature, subjects_task):
+            """Fit the model from the data.
 
-    def __init__(self, subjects, subjects_task):
+            :param subjects_feature: X,
+                    [n_samples, n_features] Matrix like object containing the subject features.
+            :param subjects_task: y,
+                    [n_samples, n_results] Matrix like object containing the subject task results.
+            """
+            betas = np.asarray(map(
+                lambda feature, task: self._get_beta(
+                    feature,
+                    task),
+                zip(subjects_feature, subjects_task)
+            ), dtype=np.float32)
+            self.beta = np.mean(betas, axis=0)
+
+        def predict(self, subjects_features):
+            """Predict the task results from the subjects features.
+
+            :param subjects_features: X,
+                        [n_subjects, n_features] Matrix like object containing the subjects features.
+            :return: y,
+                        [n_subjects, n_results] Matrix like object containing the task result prediction.
+            """
+            if self.beta is None:
+                raise BrokenPipeError("Cannot predict before the model was trained!")
+            subjects_features_with_ones = map(utils.utils.add_ones_column_to_matrix, subjects_features)
+
+            return map(lambda subject_features: subject_features @ self.beta, subjects_features_with_ones)
+
+    def __init__(self, subjects, subjects_task=None, pca=None, predictor=None):
+        """Initialize a localizer object
+
+        :param subjects: The subject to train on.
+        :param subjects_task: The subject's task result to fit the model on.
+        :param pca: The pca to use for the features extraction and ICA filtering.
+                    If not provided, you must provide subjects to create the PCA from.
+        :param feature_extractor: The feature extractor object to use.
+                Will be used to extract features from the
+        :param predictor: The predictor model to use for prediction.
+                    If not provided, a default predictor will be created, and fitted to the subjects,
+                    and the subjects' task results.
+        """
+        if pca is None:
+            raise ValueError("Cannot initialize a localizer if no pca and no subjects were provided, " +
+                             "as it cannot generate a new PCA without subjects.")
         pca = iterative_pca.iterative_pca(subjects)
-        self._feature_extractor = Localizer.FeatureExtractor(subjects, pca)
-        subject_feature = map(self._feature_extractor.extract, subjects)
-        self._predictor = Localizer.Predictor(subject_feature, subjects_task, pca)
 
-    def predict(self, x):
-        return self._predictor.predict(x)
+        self._feature_extractor = Localizer.FeatureExtractor(subjects, pca)
+        if predictor is None:
+            if subjects_task is None:
+                raise ValueError(
+                    'Cannot initialize a localizer if no predictor was provided, and no subjects and no '
+                    'subject\'s_task were provided, as it cannot train a new predictor without subjects and '
+                    'subjects\'s task results.')
+            predictor = Localizer.Predictor(pca)
+            predictor.fit(subjects, subjects_task)
+        self._predictor = predictor
+
+    def fit(self, subjects, subjects_task):
+        """Fit the current loaded model on the given data.
+
+        :param subjects: The subject to fit on.
+        :param subjects_task: The task result of each subject.
+        :return:
+        """
+        subjects_feature = map(self._feature_extractor.extract, subjects)
+        self._predictor.fit(subjects_feature, subjects_task)
+
+    def predict(self, subject):
+        """Predict the task results from the subjects features.
+
+        :param subject: The subject to predict his task results.
+        :return: The task result prediction.
+        """
+        features = self._feature_extractor.extract(subject)
+        return self._predictor.predict(features)
+
+    def save_to_file(self, file_path):
+        """Save localizer to file.
+
+        :param file_path: Path to save the object to.
+        """
+        return pickle.dump(gzip.open(file_path, 'wb'), self)
 
     @staticmethod
-    def load(file_path):
-        return pickle.load(open(file_path, 'rb'))
+    def load_from_file(file_path):
+        """Load a localizer from file.
+
+        :param file_path: File path to load from.
+        :return: The localizer object loaded.
+        """
+        res = pickle.load(gzip.open(file_path, 'rb'))
+        if not isinstance(res, Localizer):
+            raise TypeError("Content of file is either an old type and deprecated Localizer model, "
+                            "a corrupted file or in a wrong file format.")
 
 #
 # def run_all_predictions(subject_data_dir, result_dir, subjects, task_path, path_to_something):
