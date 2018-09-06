Change all function to use the same load function Now actually load once the subjects. Fixed ICA problem - now the scale is ok. but we still have numerical issues. Added jupyter notebook for testing. Change constant with fitting ICA

python/src/constants.py
@@ -5,13 +5,13 @@ struct_filename = '_Struct.dtseries.nii'
 filter_filename = 'ica_both_lowdim.dtseries.nii'
 model_filename = 'model.pcl.gzip'
 dtype = np.float32
-ICA_FUCKING_CONST = 0.0000001
-SPATIAL_FILTERS_CONST = 0.0000001
+ICA_FUCKING_CONST = 0.005
+SPATIAL_FILTERS_CONST = 0.005
 PATH_TO_SESSION = "MNINonLinear/Results"
 
 SESSION_IDS = [('1', 'LR'), ('1', 'RL'), ('2', 'LR'), ('2', 'RL')]
 PATH_TO_SESSIONS = "MNINonlinear/Results"
-SESSION_NAME_TEMPLATE = "rfMRI_REST%s_%s/rfMRI_REST%s_%s_Atlas_hp2000_clean.dtseries.nii"
+SESSION_NAME_TEMPLATE = "rfMRI_REST%s_%s/rfMRI_REST%s_%s_Atlas_MSMAll_hp2000_clean.dtseries.nii"
 SESSION_DIRS = [SESSION_NAME_TEMPLATE % (num, side, num, side) for num, side in SESSION_IDS]
 DEFAULT_TASK_FILENAME = ''
 DEFAULT_TASK_ORDERED_SUBJ_FILE = ''

python/src/iterative_pca.py
@@ -8,8 +8,9 @@ import os
 
 import numpy as np
 import sklearn.preprocessing
-from constants import dtype
+
 import utils.cifti_utils
+from constants import dtype
 
 
 #
@@ -87,7 +88,7 @@ def get_subject_rest_rfMRI_image(subject_dir, session_n, session_type):
         'rfMRI_REST{session_n}_{session_type}/' +
         'rfMRI_REST{session_n}_{session_type}' +
         '_Atlas_hp2000_clean.dtseries.nii')
-    cifti, _ = utils.cifti_utils.load_nii_brain_data_from_file(file_name)
+    cifti, _ = utils.cifti_utils.load_cifti_brain_data_from_file(file_name)
     return np.asarray(cifti, dtype=dtype)
 
 
@@ -97,7 +98,7 @@ def iterative_pca(subjects, iterative_n=1200, result_n=1000):
         for session in subject.sessions:
             res = iterative_pca_subject(
                 res,
-                session.image,
+                session.cifti,
                 iterative_n)
     return res[:result_n, :].transpose()
 

python/src/localize.py
@@ -12,7 +12,7 @@ import utils.cifti_utils
 import utils.utils
 from constants import dtype
 # TODO(loya) add full description.
-from prediction import Localizer
+from prediction import Localizer, FeatureExtractor
 
 PARSER = argparse.ArgumentParser(
     description=("""
@@ -132,7 +132,7 @@ def load_subjects_task(args, subjects):
     full_path_to_ordered_subjs = os.path.join(args.input_dir, args.task_ordered_subjects_filename)
     subj_index_dict = _get_ordered_subjects_list(full_path_to_ordered_subjs)
 
-    all_subjects_tasks, _ = utils.cifti_utils.load_nii_brain_data_from_file(full_path_to_tasks)
+    all_subjects_tasks, _ = utils.cifti_utils.load_cifti_brain_data_from_file(full_path_to_tasks)
 
     for subj in subjects:
         # TODO(loya) this might need transpose:
@@ -141,18 +141,19 @@ def load_subjects_task(args, subjects):
 
 
 # todo(kess) add optiong to also include PCA
-def train_model(subjects, subjects_task, args, pca_result=None):
+def train_model(subjects, subjects_task, args, pca_result=None, feature_extractor=None):
     """Train a localizer model
 
     :param subjects: The subjects to train on.
     :param subjects_task: The subjects' task to train on.
     :param args:
+    :param pca_result:
     :return: A localizer model
     """
     # todo(kess) this is where you change the model type.
     return Localizer(subjects, subjects_task, pca_result=pca_result,
                      load_feature_extraction=args.load_feature_extraction,
-                     feature_extraction_path=args.feature_extraction_result)
+                     feature_extraction_path=args.feature_extraction_result, feature_extractor=feature_extractor)
 
 
 def get_benchmark(localizer, subjects, subjects_task):
@@ -165,16 +166,23 @@ def get_benchmark(localizer, subjects, subjects_task):
     :return: The benchmark.
     """
     predictions = localizer.predict(subjects)
-    return (sum(list(map(lambda subject_task, prediction: np.linalg.norm(subject_task - prediction),
-                         zip(subjects_task, predictions))))).astype(dtype) / len(subjects)
+    res = []
+    for subject_task, prediction in zip(subjects_task, predictions):
+        dif = np.abs(np.array(subject_task) - np.array(prediction))
+        res.append(np.linalg.norm(dif))
+    return np.mean(np.array(res))
 
 
-def benchmark(subjects, subjects_task, args):
+def benchmark(subjects, subjects_task, args, pca_result=None):
     benchmark_list = []
+    feature_extractor = FeatureExtractor(subjects, pca_result)
+    subjects_task = np.array(subjects_task)
+    subjects = np.array(subjects)
     for train_indices, test_indices in sklearn.model_selection.LeaveOneOut().split(subjects):
+        print("Run on ", train_indices, test_indices)
         localizer = train_model(subjects[train_indices],
                                 subjects_task[train_indices],
-                                args)
+                                args, pca_result, feature_extractor)
         benchmark_list.append(
             get_benchmark(localizer,
                           subjects[test_indices],
@@ -189,11 +197,16 @@ def main():
         print("Loading subjects and tasks.")
         subjects = load_subjects(ARGS)
         subjects_task = load_subjects_task(ARGS, subjects)
+        pca_result = None
+        if not ARGS.run_with_pca:
+            pca_result_path = os.path.join(ARGS.input_dir, ARGS.pca_result)
+            pca_result, _ = utils.cifti_utils.load_cifti_brain_data_from_file(pca_result_path)
 
         if ARGS.benchmark:
             if len(subjects) <= 1:
                 raise ValueError("Not enough subjects to preform leave-one-out.")
-            mean, std, raw = benchmark(subjects, subjects_task, ARGS)
+            print("Benchmark Starting...")
+            mean, std, raw = benchmark(subjects, subjects_task, ARGS, pca_result=pca_result)
             print("Benchmark Results:")
             print("Mean:", mean)
             print("STD:", std)
@@ -201,12 +214,7 @@ def main():
 
         output_path = os.path.join(ARGS.output_dir, ARGS.output_filename)
         print("Training Model.")
-        if ARGS.run_with_pca:
-            train_model(subjects, subjects_task, ARGS).save_to_file(output_path)
-        else:
-            pca_result_path = os.path.join(ARGS.input_dir, ARGS.pca_result)
-            pca_result, _ = utils.cifti_utils.load_nii_brain_data_from_file(pca_result_path)
-            train_model(subjects, subjects_task, ARGS, pca_result=pca_result).save_to_file(output_path)
+        train_model(subjects, subjects_task, ARGS, pca_result=pca_result).save_to_file(output_path)
         print("Finished.")
 
     elif ARGS.predict and not ARGS.train:

python/tests/system_test.py
@@ -1,7 +1,7 @@
 import traceback
 
 import feature_extraction as feature_extraction
-from utils.cifti_utils import load_nii_brain_data_from_file
+from utils.cifti_utils import load_cifti_brain_data_from_file
 from utils.utils import Subject
 
 method_to_nii = [
@@ -18,7 +18,7 @@ def get_matlab_matrix_as_numpy(nii_path):
     :param nii_path: A path to the nii file.
     :return: numpy matrix with the same data.
     """
-    image, _ = load_nii_brain_data_from_file(nii_path)
+    image, _ = load_cifti_brain_data_from_file(nii_path)
     # TODO this might be more complicated: may load separately for each cifti.
     # for the ICA matrix we needed:
     # np.array(nib_data.dataobj)
@@ -39,7 +39,7 @@ def abstract_test(method_to_test, nii_path):
 
 # todo(kess) Ask Noam how to integrate with her tests.
 def run_get_subcortical_parcellation_test():
-    cifti_image, brain_models = load_nii_brain_data_from_file(
+    cifti_image, brain_models = load_cifti_brain_data_from_file(
         r'..\test_resources\GROUP_PCA_rand200_RFMRI.dtseries.nii')
     abstract_test(
         # TODO this path is not in the git. Should be added into resources
@@ -48,7 +48,7 @@ def run_get_subcortical_parcellation_test():
 
 
 def run_group_ica_separately_test():
-    cifti_image, brain_models = load_nii_brain_data_from_file(
+    cifti_image, brain_models = load_cifti_brain_data_from_file(
         r'..\test_resources\GROUP_PCA_rand200_RFMRI.dtseries.nii')
     abstract_test(
         lambda: feature_extraction.run_group_ica_separately(cifti_image, brain_models)
@@ -56,7 +56,7 @@ def run_group_ica_separately_test():
 
 
 def run_group_ica_together_test():
-    cifti_image, brain_models = load_nii_brain_data_from_file(
+    cifti_image, brain_models = load_cifti_brain_data_from_file(
         r'..\test_resources\GROUP_PCA_rand200_RFMRI.dtseries.nii')
     abstract_test(
         lambda: feature_extraction.run_group_ica_together(cifti_image, brain_models)
@@ -64,7 +64,7 @@ def run_group_ica_together_test():
 
 
 def get_semi_dense_connectome_test():
-    sc_cifti_image, _ = load_nii_brain_data_from_file(
+    sc_cifti_image, _ = load_cifti_brain_data_from_file(
         r'..\test_resources\SC_clusters.dtseries.nii')
 
     # TODO(loya) validate these are the actual files.
@@ -82,7 +82,7 @@ def get_semi_dense_connectome_test():
 
 
 def run_dual_regression_test():
-    dt, brain_models = load_nii_brain_data_from_file(
+    dt, brain_models = load_cifti_brain_data_from_file(
         r'..\test_resources\ica_LR_MATCHED.dtseries.nii')
 
     # TODO(loya) notice there are more parameters such as ROIs