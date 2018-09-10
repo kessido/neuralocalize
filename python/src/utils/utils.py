923b8876103a947fafbbe788315458e2be41a30f
noamloya <noamloya@gmail.com>
2018-09-10 20:57:19 +0300

    bugfix in fsl_glm and attempts of implementing the svd stuff

--- python/src/utils/utils.py
+++ python/src/utils/utils.py
@@ -1,3 +1,4 @@
+import scipy.sparse.linalg
 import os
 
 import numpy as np
@@ -54,10 +55,10 @@ def fsl_glm(x, y):
     dof = np.size(y, 0) - np.linalg.matrix_rank(x)
 
     sigma_sq = np.sum(r ** 2, axis=0) / dof
-
     grot = np.diag(np.linalg.inv((x.transpose().dot(x))))
     varcope = grot.reshape([len(grot), 1]).dot(sigma_sq.reshape([1, len(sigma_sq)]))
     t = beta / np.sqrt(varcope)
+    t[np.isnan(t)] = 0
     return t
 
 def fsl_demean(x, dim=None):
@@ -72,7 +73,7 @@ def fsl_demean(x, dim=None):
     dim_size = dims[dim]
     dim_rep = np.ones([len(dims)])
     dim_rep[dim] = dim_size
-    mean = np.mean(x, dim)
+    mean = np.mean(x, dim, keepdims=True)
     x = x - np.tile(mean, dim_rep.astype(dtype=int))
     return x
 
@@ -139,14 +140,16 @@ class Normalizer(object):
         dim_rep[dim] = dim_size
 
         # print(np.tile(np.mean(x, dim), dim_rep.astype(dtype=int)))
-        self.mean = np.mean(x, dim)
-        self.std = np.std(x, axis=dim, ddof=1)
+        self.mean = np.mean(x, axis=dim, keepdims=True)
+        self.std = np.std(x, axis=dim, ddof=1, keepdims=True)
         self.is_fit = True
         x = x - np.tile(self.mean, dim_rep.astype(dtype=int))
         x = x / np.tile(self.std, dim_rep.astype(dtype=int))
 
+        x[np.isnan(x)] = 0
+        x[np.isinf(x)] = 0
+        
         x = x / np.sqrt(dim_size - 1)
-        # TODO(loya) add the isnan.
         return x
 
     def normalize(self, x, dim=None):
@@ -170,3 +173,58 @@ class Normalizer(object):
         x = x / np.sqrt(dim_size - 1)
         # TODO(loya) add the isnan.
         return x
+
+    @staticmethod
+    def fsl_variance_normalize(y, n=30, threshold_a=2.3, threshold_b=0.001):
+        k = min(n, min(y.shape) - 1)
+        u, s, v = scipy.sparse.linalg.svds(y, k)
+        # u = np.array([[0, 1], [1, 0]])
+        # s = np.array([[1, 0], [0, 1]])
+        # v = np.array([[0,1],[1,0]])
+
+        print("u:", u)
+        print("S:", s)
+        print("v:", v)
+        threshold = threshold_a * np.std(v, ddof=1)
+        v[np.abs(v) < threshold] = 0
+
+        stds = np.maximum(np.std(y - u @ s @ np.transpose(v), ddof=1, axis=0), threshold_b)
+        return y / np.tile(stds, (y.shape[0], 1)) # this might need to be zero and not 1
+
+    @staticmethod
+    def fsl_svd(x, n):
+        if x.shape[0] < x.shape[1]:
+            if n < x.shape[0]:
+                eigs, d = scipy.sparse.linalg.eigs(x @ x.transpose(), n)
+                u = np.zeros([len(eigs), len(eigs)])
+                u[np.diag_indices_from(u)] = eigs
+            else:
+                eigs, d = np.linalg.eig(x @ x.transpose())
+                u = np.zeros([len(eigs), len(eigs)])
+                u[np.diag_indices_from(u)] = eigs
+                u = np.fliplr(u)
+                d = np.flipud(np.fliplr(d))
+            s = np.sqrt(np.abs(d))
+            v = x.tranpose() @ (u @ np.diag((1 / np.diag(s))))
+        else:
+            if n < x.shape[1]:
+                print("XTX:", np.ndim(x.transpose() @ x))
+                eigs, v = scipy.sparse.linalg.eigs(x.transpose() @ x, k=n)
+                d = np.zeros([len(eigs), len(eigs)])
+
+                d[np.diag_indices_from(d)] = eigs
+                print("V[:,0]:", v[:, 0])
+                print("d:", d)
+            else:
+                print("4")
+                eigs, v = np.linalg.eig(x.transpose() @ x)
+                d = np.zeros([len(eigs), len(eigs)])
+                d[np.diag_indices_from(d)] = eigs
+                v = np.fliplr(v)
+                d = np.flipud(np.fliplr(d))
+            s = np.sqrt(np.abs(d))
+            u = x @ (v @ np.diag((1 / np.diag(s))))
+        print("u:", u)
+        print("v:", v)
+        print("s:", s)
+        return u, s, v
